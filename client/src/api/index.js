d87bfe72bb8a3e8cb5dba7808fd5f4509a534534
Nirbyoleek Das <nirbyoleek152001@gmail.com>
2023-08-24 11:39:23 +0530

    URL changes

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// const url = "https://memories-full.herokuapp.com/posts";
-const url = "http://localhost:5000/posts";
+const url = "https://memories-backend-eight.vercel.app/";
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
